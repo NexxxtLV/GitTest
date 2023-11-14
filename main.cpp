75e750bdf547dad6294ac96fe7e2c53f4644aff2
NexxxtLV <59258924+NexxxtLV@users.noreply.github.com>
2023-11-14 16:12:43 +0300

    v1.1.0

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 
 void printText(std::string text)
 {
     printf(text.c_str());
 }
 
+int sumTwoDigits(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    printText("Hello World!\n");
+    printText("Hello New Feature\n");
+    printText(std::to_string(sumTwoDigits(1, 2)));
+
+    system("pause");
+    return 0;
 }
